fd8b838d5a1bcc8d7871095d42bc510108724e14
MIA1912 <102903804+MIA1912@users.noreply.github.com>
2022-04-04 07:01:24 +0700

    membuat variabel b
    menambahkan nama

--- ProjectProgramZodiak.java
+++ ProjectProgramZodiak.java
@@ -2,7 +2,9 @@ public class ProjectProgramZodiak{
   public static void main(String[] args){
   
   int a =6;
-  
+  String b = "Muhammad Ilham Aulia";
+    
+  System.out.print("Nama Saya Adalah : " + b);
   System.out.print("Nilai Nya Adalah : " + a);
 }
 }
