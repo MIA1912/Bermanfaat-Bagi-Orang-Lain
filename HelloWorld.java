2cac1448f908b0051c928b3d6ff075801d957ac6
MIA1912 <102903804+MIA1912@users.noreply.github.com>
2022-04-08 10:20:47 +0700

    Menambahkan Variabel

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author MUHAMMAD ILHAM
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        int jumbel =9;
+        System.out.println("Hello World");
+        System.out.println("Hello");
+        if(jumbel > 5){
+            System.out.println("Salah");
+        }else if(jumbel <5){
+            System.out.println("Benar");
+        }
+        
+        
+    }
+}
